b4e6dadae544d2e726b5043373a901c8b6645e31
webtunings <webtunings@gmail.com>
2014-03-27 00:29:23 +0530

    idle socket and timeout event

---
+++ idle_socket/tcpServer_idle_socket.js
@@ -0,0 +1,53 @@
+var net = require('net');
+
+var socketUtil = require("./socketUtil");
+
+var tcpServer = net.createServer(function(socket){
+		console.log('connection established....');
+		
+		socketUtil.socketAddress(socket);
+		
+
+		tcpServer.getConnections(function(error,count){
+			console.log('number of concurrent tcp connections= '+ count);
+			});	
+	
+		socket.on('end', function(){
+			socketUtil.socketStats(socket);
+			console.log('server disconnected..');
+			});
+		
+		socket.on('close', function(){
+			console.log('closed event on socket fired');
+			});
+	
+		socket.on('data', function(data){
+			console.log('data received from the tcp client');
+			socket.write('Server Reply: ' + data);
+			//socketUtil.socketStats(socket);
+			//socket.emit('error', new Error('forcefully injected error'));
+			});		
+
+		socket.on('error', function(error){
+			console.log('something wrong happpened here');
+			//socket.end('socket can send some more data but it will be ended');
+			socket.destroy();
+			});
+		
+		socket.setTimeout(20000,function(){ socket.end('disconnecting client...');});
+
+		});
+
+tcpServer.maxConnections=10;
+
+
+//setTimeout(function(){ tcpServer.close(function(){console.log('server closed');})},60000);
+			
+tcpServer.on('close',function(){ console.log('second server close event handler');});
+
+tcpServer.listen(function(){ 
+		var port = tcpServer.address().port;
+		console.log('server started listening on port: ' + port);
+		});
+
+
