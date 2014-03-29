f92c7ee60b5aff25a76b4a0e70a2f910535f187c
webtunings <webtunings@gmail.com>
2014-03-29 22:24:15 +0530

    basic tcp chat application

---
+++ tcpChat.js
@@ -0,0 +1,35 @@
+var net = require("net");
+
+
+var sockets = [];
+
+var tcpServer = net.createServer();
+
+tcpServer.on('connection', function(socket){
+	console.log('connection established');
+	
+	socket.setEncoding('utf8');
+
+	sockets.push(socket);
+
+	socket.on('data', function(data){
+	console.log(data);
+
+	var clients = sockets.length;
+	
+	for(var i=0; i < clients;i++){
+	if(sockets[i] === socket) continue;
+	sockets[i].write(data);
+	}
+
+	});
+
+
+
+	socket.on('end', function(){
+	sockets.splice(sockets.indexOf(sockets),1);
+	});
+
+});
+
+tcpServer.listen(8000);
