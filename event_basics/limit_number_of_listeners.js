1e096b09a3f9dcea73c66cd6a5cabcc968c33771
webtunings <webtunings@gmail.com>
2014-04-12 18:12:31 +0530

    limit the number of event listeners

---
+++ event_basics/limit_number_of_listeners.js
@@ -0,0 +1,14 @@
+var net = require("net");
+
+var tcpServer = net.createServer();
+
+tcpServer.on('connection', function(socket){
+	
+	socket.setMaxListeners(1);
+	socket.on('pipe', function(){console.log('pipe event');});
+	socket.on('pipe', function(){ setTimeout(function(){ socket.unpipe(socket);},30000);});	
+	console.log(socket.listeners('pipe')[1].toString());
+	socket.pipe(socket);
+});
+
+tcpServer.listen(8000);
